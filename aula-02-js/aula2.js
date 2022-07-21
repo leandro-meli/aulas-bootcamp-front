a39304b6454982a8a6167c189309b49f74312bf6
Leandro Hespanhol <leandro.hespanhol@mercadolivre.com>
2022-07-21 17:03:30 -0300

    exercicios 1 a 5

---
+++ aula-02-js/aula2.js
@@ -0,0 +1,14 @@
+function calcularPreco(nome, preco) {
+  let custo = 700.00;
+  if (preco < 2001) {
+    custo = 300.00;
+  }
+  if (preco < 4001) {
+    custo = 450.00;
+  }
+  console.log(`O produto ${nome} custa R$${preco.toFixed(2)}. Seu custo de envio é R$${custo.toFixed(2)}. Portanto, o preço final é R$${(preco + custo).toFixed(2)}`);
+}
+
+calcularPreco('Macbook', 2500)
+calcularPreco('Celular', 500)
+calcularPreco('Playstation', 4500)
